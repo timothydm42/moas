947f136b808102b5ad08e31bfb8064d6d609dbc3
Mikhail Collins <kyleryker@gmail.com>
2017-05-01 11:59:33 -0600

    built server, database and connected

---
+++ node.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const massive = require('massive');
+const cors = require('cors');
+const massiveCtrl = require('./src/controllers/massiveCtrl.js');
+const app = express();
+
+app.use(cors());
+
+app.use(express.static('build'))
+
+app.get('/inventory', massiveCtrl.getDb);
+
+app.listen(3002, () => {console.log('listening on port 3002')});
