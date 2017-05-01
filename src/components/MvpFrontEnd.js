6fd7bad0aeaf0331fe1f869b74b4e83096d23ae0
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-05-01 10:12:44 -0600

    rebuilt front end

---
+++ src/components/MvpFrontEnd.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class mvpFrontEnd extends Component {
+  render() {
+    return (
+      <div>
+        Sup world? Yo.
+      </div>
+    )
+  }
+}
