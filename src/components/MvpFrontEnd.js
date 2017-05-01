e0648300cf0e41e6747b71270ffeaae0fc8ab1f4
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-05-01 10:45:51 -0600

    put on a fake navbar

--- src/components/MvpFrontEnd.js
+++ src/components/MvpFrontEnd.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-export default class mvpFrontEnd extends Component {
+export default class MvpFrontEnd extends Component {
   render() {
     return (
       <div>
