a44391dfb8ca3e8d997259c9bea1695223611931
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-05-02 10:33:15 -0600

    added a lot more variability in alexa commands with mikhail pair programming

--- alexaFiles/intents.js
+++ alexaFiles/intents.js
@@ -1,7 +1,39 @@
 {
   "intents": [
     {
-        "intent": "IncrementLemons"
+      "intent": "Increment",
+      "slots": [
+        {
+          "name": "item",
+          "type": "AMAZON.Food"
+        },
+        {
+          "name": "num",
+          "type": "AMAZON.NUMBER"
+        }
+      ]
     },
+    {
+      "intent": "Decrement",
+      "slots": [
+        {
+          "name": "item",
+          "type": "AMAZON.Food"
+        },
+        {
+          "name": "num",
+          "type": "AMAZON.NUMBER"
+        }
+      ]
+    },
+    {
+      "intent": "GetInventory",
+      "slots": [
+        {
+          "name": "item",
+          "type": "AMAZON.Food"
+        }
+      ]
+    }
   ]
 }
