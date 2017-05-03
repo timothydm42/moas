f4b561d001347c6a5eca4f6278ad2019aec9be19
Mikhail Collins <kyleryker@gmail.com>
2017-05-03 16:37:53 -0600

    error handling to make sure socket works

--- src/controllers/massiveCtrl.js
+++ src/controllers/massiveCtrl.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const massive = require('massive');
-<<<<<<< HEAD
 const secret = require('../../.config.js')
 const app = express();
 const http = require('http').Server(app);
@@ -36,22 +35,3 @@ io.on('connection', (socket) => {
         });
     });
 });
-=======
-const secret = require('../../.config.js');
-
-const db =  massive.connectSync({
-  connectionString : "postgres://"+secret.dbUsername+":"+secret.dbPassword+"@"+secret.dbEndpoint
-});
-
-exports.getDb = (req, res, next) => {
-  db.run('select * from inventory', (err, database) =>{
-    res.send(database);
-  });
-};
-
-exports.incrementProduct = (req, res, next) => {
-  db.run("update inventory set quantity = (quantity + 1) where productname = $1", [param1], (err,result) => {
-    res.send(database);
-  });
-}
->>>>>>> master
