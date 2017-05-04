50c2bb68ff31b279963b1cebee3c001d423e5ac5
Mikhail Collins <kyleryker@gmail.com>
2017-05-04 13:47:35 -0600

    added some contact info and an about page

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+<p>Github Accounts:
+Mikhail: https://github.com/kyleryker
+Nathan: https://github.com/Nathaniel-j-s
+Tim: https://github.com/timothydm42
+</p>
+<p>
+  LinkedIn Accounts:
+  Mikhail: https://www.linkedin.com/in/mikhail-collins-87a85b13a/
+  Nathan: https://www.linkedin.com/in/nathaniel-smith-122962138/
+  Tim: Timothy Mitchell: Master of World Leadership
+</p>
