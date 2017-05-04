b3cf3f9e820f64898d2eda5f1219ca89ed99f67f
Mikhail Collins <kyleryker@gmail.com>
2017-05-04 15:51:27 -0600

    prepared for merge

--- src/components/About.js
+++ src/components/About.js
@@ -1,2 +1,1 @@
-<p> This page is designed to be used in tandem with an amazon echo device. It tracks your inventory, which you can either manage here or modify and hear via alexa. This adds ease of use and saved time when businesses must constantly be modifying the inventory and now won't always have to be at a computed to do so.
-Any Questions can be brought to Mikhail, Nate, or Tim.</p>
+
