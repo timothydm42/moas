5bbee1709e371ee8886a1c43d99796599db83178
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-05-10 09:00:49 -0600

    about page is pretty close to done

--- src/components/About.js
+++ src/components/About.js
@@ -6,36 +6,34 @@ export default class About extends Component {
     const styles = this.getStyles();
     return (
       <div style={styles.aboutPage}>
-        <p>
-          What does this application do?
-        </p>
-        <p>
-          This application helps you maintain your inventory. It does this uniquely with verbal commands, using Amazon Alexa devices.
-        </p>
-        <div className="row" style={styles.about}>
-          <p className="col-md-4" style={styles.information}><strong>Problem:</strong> Tedious, error prone data entry
-            No more keeping notes in your head or elsewhere, and then sitting down to your inventory and manually entering in each change; simply inform alexa when inventory changes, and she takes care of updating your records.
-            Example: Your restaurants are using their stock all day long, sometimes in a crunch employees keeping track of inventory during a rush fails
-            Your employees constantly have to re-count inventory once inventory is finally re-calculated, your employee has to sit down and enter it manually somewhere to keep track of it.
-            This tedious, error prone, lengthy process is repeated.
-          </p>
-          <p className="col-md-4" style={styles.information}><strong>Solution:</strong> You install an Alexa Device next to your pantry and refrigerators.
-            As stock is used, your employees simply tell Alexa what they are using as they pass by.
-            Alexa updates a cloud based inventory.
-            The cloud based inventory is accessible online for convenient viewing.
-            Including live updates sent by Alexa you can update the cloud based inventory manually as well.
-          </p>
-          <ul className="col-md-3" style={styles.information}><h4 style={styles.techs}><strong>Web technologies:</strong></h4>
+        <div style={styles.short}>
+          <p style={styles.shortStart}><strong>In short, what does this application do?</strong></p>
+          <p style={styles.shortEnd}>This application helps you maintain your inventory. It does this uniquely with verbal commands, using Amazon Alexa devices.</p>
+        </div>
+        <div style={styles.problem}><h3 style={styles.problemTitle}><strong>The Problem</strong></h3>
+          <p style={styles.problemStart}><strong>Tedious data entry, prone to error based on several factors.</strong></p>
+          <p style={styles.problemRest}><strong>Let's face it.</strong> We forget to handle inventory changes sometimes. In a work environment where stock is changing all day long, things can be missed. And it can take a lot of time to update everything at the end of the day.</p>
+        </div>
+        <div style={styles.solution}><h3 style={styles.solutionTitle}><strong>Our Solution</strong></h3>
+          <p style={styles.solutionStart}><strong>Hands free, verbal commands.</strong></p>
+          <p style={styles.solutionRest}>With an Amazon Alexa placed near your physical inventory, you can ask it to update your stock, in or out.</p>
+        </div>
+        <div style={styles.features}>
+          <p>Features</p>
+          <p>Verbally add or subtract products from your stock.</p>
+          <p>Check your inventory in passing if you don't have the time to look.</p>
+          <p>Add or remove product lines from your inventory through your Alexa as well.</p>
+          <p>The website interface allows you to do all of this manually.</p>
+          <p>Being supported by Amazon means you can easily verify what your Alexa hears when you speak to it.</p>
+          <p>Additionally, you can receive notifications through your phone whenever your Alexa is used, keeping you up to date at all times.</p>
+        </div>
+        <div style={styles.tech}>
+          <ul style={styles.information}><h4 style={styles.techs}><strong>Web Technologies:</strong></h4>
             <li><strong>Client:</strong> React library, React-Router, Axios</li>
             <li><strong>Server:</strong> Node, Express, Massive</li>
-            <li><strong>Database:</strong> Postgresql AWS Remote Database FAAS</li>
+            <li><strong>Database:</strong> Postgresql, AWS Remote Database, FAAS</li>
             <li><strong>Alexa</strong> programmed with Amazon Lambda functions</li>
           </ul>
-        </div>
-        <div className="row" style={styles.about}>
-          <p className="col-md-4" style={styles.information}>This is an inventory management system built to function with the Amazon Alexa line of products. With this service, you will be able to handle your inventory with voice commands, allowing you to do what you need to do, when you need to do it.</p>
-          <p className="col-md-4" style={styles.information}>Update inventory, hands free, without the usual tedious data entry. We do provide an interface for tedious data entry too, in case you want it.</p>
-          <p className="col-md-3" style={styles.information}>Note: Your Alexa already hates the number 2. That's not our fault.</p>
         </div>
       </div>
     )
@@ -49,15 +47,82 @@ export default class About extends Component {
         height: "100vh",
         padding: "15px"
       },
-      about: {
+      short: {
+        position: "absolute",
+        top: "65px",
+        right: "150px",
+        width: "25%",
         backgroundColor: "#88BBD6",
+        opacity: 0.8,
+        color: "white",
         border: "1px solid white",
         borderRadius: "5px",
+        padding: "5px"
+      },
+      shortStart: {
+        margin: 0,
+        padding: 0
+      },
+      shortEnd: {
+        margin: 0,
+        padding: 0
+      },
+      problem: {
+        position: "absolute",
+        top: "68px",
+        left: "20px",
+        width: "40%",
+        backgroundColor: "rgb(116, 116, 116)",
+        border: "1px solid black",
+        borderRadius: "50px 80px 30px 8px",
+        boxShadow: "3px 3px 3px rgb(46, 46, 46)",
+        opacity: 0.8,
+        color: "black",
+        textAlign: "center"
+      },
+      problemTitle: {
+        padding: 0,
+        paddingTop: "8px",
+        margin: 0
+      },
+      problemStart: {
+        fontSize: "105%",
+        margin: "3px",
+        padding: 0
+      },
+      problemRest: {
+        fontSize: "80%",
+        padding: "3px"
+      },
+      solution: {
+        position: "absolute",
+        top: "164px",
+        left: "335px",
+        width: "50%",
+        backgroundColor: "rgb(215, 215, 215)",
+        border: "1px solid white",
+        borderRadius: "64px 48px 20px 28px",
         boxShadow: "3px 3px 3px rgb(245, 245, 245)",
-        opacity: 0.85,
+        opacity: 0.8,
         color: "white",
-        width: "98%",
-        margin: "0 auto"
+        textAlign: "center"
+      },
+      solutionTitle: {
+        color: "white",
+        padding: 0,
+        margin: "10px 0px 5px 0px"
+      },
+      solutionStart: {
+        fontSize: "115%",
+        margin: 0,
+        paddingTop: "10px"
+      },
+      solutionRest: {
+        fontSize: "105%",
+        padding: "5px 30px 3px 30px"
+      },
+      tech: {
+
       },
       information: {
         margin: "10px"
