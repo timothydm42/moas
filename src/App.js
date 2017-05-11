a91d31ec01cce3eae175bce25895a91d4a535290
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-05-11 11:40:41 -0600

    auth starting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Home from './components/Home';
 import Inventory from './components/Inventory/Inventory';
 import About from './components/About';
 import Contact from './components/Contact';
 
+import Login from './components/Login';
+import AuthService from './utils/AuthService';
+
+const auth = new AuthService('Qw7Pr6d0lWqyDldL8TDFYikzGMzWIQ4L', 'nathaniel-j.auth0.com');
+
+// validate authentication for private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' })
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router auth={auth}>
           <div>
             <Navbar />
-            <Redirect from="/" to="/about"/>
+            <Redirect from="/" to="/home"/>
             <Route path="/home" component={Home}/>
-            <Route path="/inventory" component={Inventory}/>
+            <Route path="/inventory" component={Inventory} onEnter={requireAuth}/>
+            <Route path="/login" component={Login}/>
             <Route path="/about" component={About}/>
             <Route path="/contact" component={Contact}/>
           </div>
