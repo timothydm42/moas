5ba8cce238d02bca62d021906185ed4b500b6600
Nathan <nathaniel.j.mith@gmail.com>
2017-05-18 17:36:35 -0600

    Update Login.js
    Commented out react bootstrap because it's no longer being used and causing problems.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ButtonToolbar, Button} from 'react-bootstrap'
+// import {ButtonToolbar, Button} from 'react-bootstrap'
 import AuthService from '../utils/AuthService'
 // import styles from './styles.module.css'
 
